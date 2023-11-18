2b9d8f45dc22a1a46fc9d8b55f61fd1093f00242
Vinicius Costa <vinicius.fonseca132@gmail.com>
2023-11-17 21:23:18 -0300

    some updates on visual

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import { Card } from './components/card/card';
 import { FoodData } from './interface/foodData';
 import { useFoodData } from './hooks/useFoodData';
@@ -7,28 +7,35 @@ import { CreateModal } from './components/card/create-modal/create-modal';
 
 function App() {
   const { data } = useFoodData();
-  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleOpenModal = () => {
-    setIsModalOpen(prev => !prev)
+      setIsModalOpen(prev => !prev);
+  }
+
+  const handleBuy = (foodTitle: string) => {
+      // Lógica de compra ou ação desejada ao clicar no botão "Comprar"
+      console.log(`Item "${foodTitle}" comprado.`);
   }
 
   return (
       <div className="container">
-        <h1>Cardápio</h1>
-        <div className="card-grid">
-          {data?.map(foodData =>
-          <Card
-           price={foodData.price} 
-           title={foodData.title} 
-           image={foodData.image}
-           />
-           )}
-        </div>
-        {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
-        <button onClick={handleOpenModal}>Novo</button>
+          <h1>Cardápio</h1>
+          <div className="card-grid">
+              {data?.map(foodData =>
+                  <Card
+                      key={foodData.title}
+                      price={foodData.price}
+                      title={foodData.title}
+                      image={foodData.image}
+                      onBuyClick={() => handleBuy(foodData.title)}
+                  />
+              )}
+          </div>
+          {isModalOpen && <CreateModal closeModal={handleOpenModal} />}
+          <button id ="botaoModal" onClick={handleOpenModal}>Novo</button>
       </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,4 +1,4 @@
-import './card.css'
+import './card.css';
 
 interface CardProps {
     price: number,
@@ -6,12 +6,20 @@ interface CardProps {
     image: string
 }
 
-export function Card({price, image, title} : CardProps) {
-    return(
+interface CardWithButtonProps extends CardProps {
+    onBuyClick: () => void;
+}
+
+export function Card({ price, image, title, onBuyClick }: CardWithButtonProps) {
+    return (
         <div className="card">
-            <img src={image}/>
+            <img src={image} alt={title} />
             <h2>{title}</h2>
-            <p><b>Valor:</b>{price}</p>
+            <p><b>Valor:</b> {price}</p>
+            <div>
+            <button id="edit" onClick={() => alert('Botão de editar clicado')}title="Editar Item">Editar</button>
+            <button id="delete" onClick={() => alert('Botão de deletar clicado')}title="Deletar Item">Deletar</button>
+            </div>
         </div>
     )
 };
